89989234937a4fa9394ba21f0a473f85b0c65011
Neshnjenga <chegenelson641@gmail.com>
2024-10-04 09:39:38 +0300

    My second Flask commit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key='ftdjiutcfxdcvcvytftstcd'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']=465
 app.config['MAIL_USERNAME']='chegenelson641@gmail.com'
-app.config['MAIL_PASSWORD']='cfukaomufqorewgu'
+
 app.config['MAIL_USE_TSL']=False
 app.config['MAIL_USE_SSL']=True
 
